7d6a16b0afb61271f142cd7371e2d309e06370ef
enamul58 <enamul58@live.com>
2019-07-19 07:02:48 +0600

    add new file

---
+++ js/my-script.js
@@ -0,0 +1,20 @@
+
+ $('#img1').click(function () {
+    var imgSrc = $(this).attr('src');
+    $('#mainImg').attr('src', imgSrc);
+});
+
+ $('#img2').click(function () {
+     var imgSrc = $(this).attr('src');
+     $('#mainImg').attr('src', imgSrc);
+ });
+
+ $('#img3').click(function () {
+     var imgSrc = $(this).attr('src');
+     $('#mainImg').attr('src', imgSrc);
+ });
+
+ $('#img4').click(function () {
+     var imgSrc = $(this).attr('src');
+     $('#mainImg').attr('src', imgSrc);
+ });
